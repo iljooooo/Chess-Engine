edadd8d37e8fb7eb19cec68007acf2723137a63b
iljooooo <simoneserena04@gmail.com>
2025-12-15 12:12:05 +0100

    Reorganized files and deletede tests folder

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import src.ChessMain as ChessMain
 import src.ChessEngine as ChessEngine
 import pygame as p
-from src.UI import GeneralMenu, Button, Text, NewGameMenu, SettingsMenu
+from src.utils_interfaces.UI import Button
 
 
 WIDTH = HEIGHT = 512
@@ -12,43 +12,6 @@ def main():
     screen = p.display.set_mode((WIDTH, HEIGHT))
     #clock = p.time.Clock()
     #screen.fill(p.Color('white'))
-
-    menu = GeneralMenu(
-        screen=screen,
-        bg_color='black',
-        elements=[
-            Button(
-                x = 100, 
-                y = 100, 
-                color='white',
-                text='NEW GAME',
-                width=100,
-                height=20,
-                callback = lambda:0
-            ),
-            
-            Button(
-                x = 100,
-                y = 200,
-                color = 'white',
-                text='SETTINGS',
-                width=100,
-                height=20,
-                callback = lambda: 0
-            ),
-
-            Button(
-                x = 100,
-                y = 200,
-                color = 'white',
-                text = 'CREDITS',
-                width = 100,
-                height = 20,
-                callback= lambda: 0
-            )
-                
-        ]
-    )
 ##
 
 if __name__ == '__main__':
---
+++ src/infterfaces.py
@@ -0,0 +1,13 @@
+from utils_interfaces.UI import Button
+
+class OpenMenu():
+    pass
+##
+
+class SettingsMenu():
+    pass
+##
+
+class GameStartingMenu():
+    pass
+##
